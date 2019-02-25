0011c9ced8311314d75b07f262eea01a262471f4
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-25 20:38:47 +0100

    Define dynamic routes through Express

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
  const express = require('express')
  const app = express()
 
-app.get('/', (_req, res) => {
-  res.send('Hello World 🚀')
+ const fs = require('fs')
+ const _ = require('lodash')
+ const users = []
+
+ fs.readFile('users.json', { encoding: 'utf8' }, (err, data) => {
+   if (err) throw err
+
+   JSON.parse(data).forEach(user => {
+     user.name.full = _.startCase(`${user.name.first} ${user.name.last}`)
+     users.push(user)
+   })
+ })
+
+ app.get('/', (_req, res) => {
+  let buffer = ''
+
+  users.forEach(user => {
+    buffer += `<a href="/user/${user.username}">${user.name.full}</a><br>`
+  })
+
+  res.send(buffer)
 })
 
-app.get('/yo', (_req, res) => {
-  res.send('YO 🔥')
+app.get(/big.*/, (_req, _res, next) => {
+  console.log('It is very big 💪')
+  next()
+})
+
+app.get('/user/:username', (req, res) => {
+  const { username } = req.params
+  const user = users.find(user => user.username === username)
+
+  const userHMTL = `
+    <h1>${user.name.title} ${user.name.full}</h1>
+  `
+
+  res.send(userHMTL)
 })
 
 const server = app.listen(3000, () => {
